1c55bf83dc8408ef5dcd928e439805a511dbd285
joel lloyd Vincent Bacus <joellloydFL@gmail.com>
2026-03-27 00:42:01 +0800

    Add session tracking and autosave list UI

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QStackedWidget
 from PySide6.QtCore import Qt
 from pages.settings.settings_page import SettingsPage
 from pages.table.table_page import TablePage
-
+import uuid
 
 def resource_path(relative_path):
     if hasattr(sys, "_MEIPASS"):
@@ -14,11 +14,14 @@ def resource_path(relative_path):
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        self.current_session_id = self.generate_session()
         self.setWindowTitle("Lead Helper")
         self.resize(600, 600)
         self.stack = QStackedWidget()
         self.settings_page = SettingsPage(self.show_home, self.handleFileUpload)
         self.home_page = TablePage()
+        self.settings_page.set_session_id(self.current_session_id)
+        self.home_page.set_session_id(self.current_session_id)
         self.stack.addWidget(self.settings_page)
         self.stack.addWidget(self.home_page)
         layout = QVBoxLayout()
@@ -27,15 +30,26 @@ class MainWindow(QWidget):
         layout.addWidget(self.stack)
         self.setWindowFlag(Qt.WindowType.WindowStaysOnTopHint, True)
         self.setLayout(layout)
+
+
     def handleFileUpload(self, rows):
+        self.current_session_id = self.generate_session()
+        self.settings_page.set_session_id(self.current_session_id)
+        self.home_page.set_session_id(self.current_session_id)
         self.home_page.load_data(rows)
         self.show_home()
+
 
     def show_home(self):
         self.stack.setCurrentWidget(self.home_page)
 
+
     def show_settings(self):
         self.stack.setCurrentWidget(self.settings_page)
+
+
+    def generate_session(self) -> str:
+        return uuid.uuid4().hex[:8]
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- pages/table/table_page.py
+++ pages/table/table_page.py
@@ -1,40 +1,56 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
 from components.Button import CustomButton
+from PySide6.QtCore import Qt
 from mainTable.fileTable import FileTable
 class TablePage(QWidget):
     def __init__(self):
         super().__init__()
+        self.current_session_id = None
         layout = QVBoxLayout(self)
+        self.session_text = QLabel("")
+        self.session_text.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        layout.addWidget(self.session_text)
         layout.addWidget(QLabel("Home Page"))
-        btn_copy_number_rows = CustomButton("Copy numbers", "secodary")
+        btn_copy_number_rows = CustomButton("Copy numbers", "secondary")
         self.table = FileTable()
         btn_copy_number_rows.clicked.connect(self.table.copy_numbers_from_table)
         layout.addWidget(self.table)
         layout.addWidget(btn_copy_number_rows)
 
-    def normalize(self, text):
+    def normalize_text(self, text):
         return " ".join(text.replace("\xa0", " ").split()).lower()
     
-    def build_full_row(self, row):
-        return self.normalize(
+    def build_address(self, row):
+        return self.normalize_text(
             f"{row.get('Mailing Address', '')} "
             f"{row.get('Mailing Unit #', '')} "
             f"{row.get('Mailing City', '')} "
             f"{row.get('Mailing State', '')} "
             f"{row.get('Mailing Zip', '')}"
         )
+    def set_session_id(self, session_id):
+        self.current_session_id = session_id
+        self.session_text.setText(self.current_session_id)
+        self.table.set_session_id(session_id)
+        
     def load_data(self, rows):
         GLOBAL_CURRENT_FUNCTION = "load_data()"
         lastalgo = "0"
+        
         try:
+            lastalgo = "1"
             data = []
             for r, row_data in enumerate(rows):
-                address_norm = self.build_full_row(row_data)
+                lastalgo = "2"
+                address_norm = self.build_address(row_data)
                 if len(row_data.get('Owner 2 First Name')) and len(row_data.get('Owner 2 Last Name')):
+                    lastalgo = "3"
                     for x in range(2):
+                        lastalgo = "4"
                         data.append((f"{row_data.get(f'Owner {x + 1} First Name')} " f"{row_data.get(f'Owner {x + 1} Last Name')}", 
                                      address_norm, ''))
                 else:
+                    lastalgo = "5"
                     data.append((f"{row_data.get('Owner 1 First Name')} " f"{row_data.get('Owner 1 Last Name')}", 
                                      address_norm, ''))
             self.table.process_data(data)
